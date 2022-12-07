ecbc4882100e279740bef2ffdd71e92619830165
vivek.shukla@cgi.com <vivek.shukla@cgi.com>
2022-12-06 20:00:41 -0600

    committing the code

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component(
+    {
+        selector:'app-success-alert',
+        styleUrls : ['./success-alert.component.css'],
+        templateUrl:'./success-alert.component.html'
+    }
+)
+export class SuccessAlertComponent{
+
+}
